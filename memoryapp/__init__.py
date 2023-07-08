aeca2a7229e766085c7ed0e34af0a144e1f192ad
jajkiewi <izabela.jajkiewicz@gmail.com>
2023-07-08 10:39:19 +0200

    Konfiguracja i uruchomienie.

---
+++ memoryapp/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+# Instancja klasy Flask
+app = Flask(__name__)  # nazwa domyślnego pakietu
+cors = CORS(app, resources={r"/*": {"origins": "*"}})  # r"" - oznacza, że jest to format "row"
+app.debug = True  # W terminalu wyświetli się każde odpytanie, które trafiać będzie do aplikacji
+
+
+"""
+ZMNIEJSZENIE RESTRYKCJI
+{"origins": "*"}} - obojętnie, z jakiego źródła będzie pochodziło zapytanie, serwer będzie przepuszczał
+"""
---
+++ run.py
@@ -0,0 +1,7 @@
+# Doinstalowanie bibliotek, które będą wykorzystywane w aplikacji
+
+from memoryapp import app  # z pakietu "memoryapp" import "app"
+
+if __name__ == '__main__':
+    app.run()
+
